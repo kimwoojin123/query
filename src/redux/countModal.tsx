0ef111eccfd3cc8e02d038dd5cdddb36b4c71172
ciccommunity <cic@ciccommunityui-iMac.local>
2024-03-22 12:32:07 -0700

    useModal훅 사용

--- src/redux/countModal.tsx
+++ src/redux/countModal.tsx
@@ -3,31 +3,28 @@ import { RootState } from './store';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { openModal, closeModal } from './reducer/modal.reducer';
+import { useModal } from '../hooks/useModal';
 
 export default function CountModal(): JSX.Element {
+	const { open, close } = useModal();
 	const dispatch = useDispatch();
 	const count = useSelector((state: RootState) => state.counter?.value);
 	const isCountModalOpen = useSelector((state: RootState) => state.modal['countModal']?.isOpen);
 
 	useEffect(() => {
 		if (count && count > 10) {
-			dispatch(openModal({ modalId: 'countModal' }));
+			open('countModal');
 		} else {
-			dispatch(closeModal({ modalId: 'countModal' }));
+			close('countModal');
 		}
 	}, [count, dispatch]);
 
 	return (
-		<CustomModal
-			isOpen={isCountModalOpen}
-			onRequestClose={() => dispatch(closeModal({ modalId: 'countModal' }))}
-			width="400px"
-			height="200px"
-		>
+		<CustomModal isOpen={isCountModalOpen} onRequestClose={() => close('countModal')} width="400px" height="200px">
 			<div>
 				<h2>Title</h2>
 				<p>Modal Content</p>
-				<button onClick={() => dispatch(closeModal({ modalId: 'countModal' }))}>Close Modal</button>
+				<button onClick={() => close('countModal')}>Close Modal</button>
 			</div>
 		</CustomModal>
 	);
